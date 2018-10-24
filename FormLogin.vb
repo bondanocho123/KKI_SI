699bac54112999efeb41958612b78736f6049f37
AFTSoftware <bondanocho123@yahoo.com>
2018-10-25 03:02:10 +0600

    Penambahan Database

--- FormLogin.vb
+++ FormLogin.vb
@@ -1,3 +1,12 @@
 ﻿Public Class FormLogin
 
+    Private Sub btnLogin_Click(sender As System.Object, e As System.EventArgs) Handles btnLogin.Click
+        Dim CUSysPar As New CUserSystemParameter
+
+        If CUSysPar.GetLogUserSystemParameter(CUSysPar, txtUsername.Text, txtPassword.Text) Then
+            MsgBox("Selamat Datang Admin")
+        Else
+            MsgBox("Password Salah !!")
+        End If
+    End Sub
 End Class
